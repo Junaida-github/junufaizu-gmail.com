195dea199cf76cd0c4d863c74db386710d1f012e
Junaida-github <62962290+Junaida-github@users.noreply.github.com>
2020-04-01 01:44:35 +0530

    App_Start

---
+++ BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace OnlineUsrToDoLst.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Scripts/vendor/bootstrap/css/bootstrap.min.css",
+                       "~/Content/fonts/font-awesome-4.7.0/css/font-awesome.min.css",
+                       "~/Content/fonts/Linearicons-Free-v1.0.0/icon-font.min.css",
+                       "~/Scripts/vendor/animate/animate.css",
+                       "~/Scripts/vendor/css-hamburgers/hamburgers.min.css",
+                       "~/Scripts/vendor/select2/select2.min.css",
+                       "~/Content/css/util.css",
+                       "~/Content/css/main.css"
+                       ));
+
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/vendor/jquery/jquery-3.2.1.min.js",
+                        "~/Scripts/vendor/bootstrap/js/popper.js",
+                        "~/Scripts/vendor/bootstrap/js/bootstrap.min.js",
+                        "~/Scripts/js/main.js"
+                        ));
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(OnlineUsrToDoLst.App_Start.Startup))]
+namespace OnlineUsrToDoLst.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+           // app.MapSignalR();
+            CookieAuthenticationOptions options = new CookieAuthenticationOptions();
+            options.AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie;
+            options.LoginPath = new PathString("/account/login");
+            app.UseCookieAuthentication(options);
+        }
+    }
+}
